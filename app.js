3b4fda84e758291cc17e0e7c68a33a87b40348a9
DevSouvaniK <155004942+DevSouvaniK@users.noreply.github.com>
2024-01-20 17:10:53 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelectorAll("#reset-bth");
+
+let turn0 = true; // playerX playerO
+
+// Winning pattens
+
+//2d arry
+const winPatterns = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log("box was clicked");
+        if(turn0) {
+            box.innerText = "O";
+            turn0 = false;
+        } else {
+            box.innerText = "X";
+            turn0 = true;
+        }
+
+
+        
+    })
+})
+
